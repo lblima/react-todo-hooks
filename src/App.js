66dabc3f761cd1dcf6a8eeba631cac5b6fb199c6
Leonardo Bentes <leonardo.bentes@sefaz.ce.gov.br>
2018-11-29 09:57:19 -0300

    todo app sample using hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useContext, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function appReducer(state, action) {
+  switch (action.type) {
+    case "reset":
+      return action.payload;
+    case "add": 
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: '',
+          completed: false
+        }
+      ];
+    case "delete":
+      return state.filter(item => item.id !== action.payload);
+    case "complete":
+    return state.map(item => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }
+
+        return item;
+      }
+     );
+    default:
+      break;
   }
 }
 
-export default App;
+const Context = React.createContext();
+
+export default function AppTodoHook() {
+    const [state, dispatch] = useReducer(appReducer, []);
+
+    useEffect(() => {
+      const raw = localStorage.getItem("data");
+      dispatch({ type: "reset", payload: JSON.parse(raw) });
+    }, []);
+    
+    useEffect(() => {
+      localStorage.setItem("data", JSON.stringify(state));
+    }, [state]);
+
+    return (
+      <Context.Provider value={dispatch}>
+        <div className="App">
+            <h3>todo hooks</h3>
+            <button onClick={() => dispatch({ type: "add" })}>New Item</button>
+            <br />
+            <br />
+            <TodoList items={state} />
+        </div>
+      </Context.Provider>
+    );
+}
+
+function TodoList({ items }) {
+  return <div style={{ width: "300px" }}>
+          { items.map(item => <TodoItem key={ item.id } {...item} />) }
+        </div>
+}
+
+function TodoItem({ id, text, completed }) {
+  const dispatch = useContext(Context);
+  return (
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+      <input type="checkbox" checked={completed} onChange={() => dispatch({ type: "complete", payload: id })} />
+      <input type="text" defaultValue={text} />
+      <button onClick={() => dispatch({ type: "delete", payload: id })}>Delete</button>
+    </div>
+  )
+}
